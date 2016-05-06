90f6384acea64bcfb87e63b30079a430110b49ee
LeugimRapsag <miguelgaspar@msn.com>
2016-05-06 13:04:29 +0100

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -1,1 +1,21 @@
-#
+## Preparation
+setwd("C:/Coursera/DataScientitist's/EDA/Data")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+NEISCC <- merge(NEI, SCC, by="SCC")
+library(ggplot2)
+
+## code of plot6.R
+subsetNEI <- NEI[(NEI$fips=="24510"|NEI$fips=="06037") & NEI$type=="ON-ROAD",  ]
+aggregatedTotalByYearAndFips <- aggregate(Emissions ~ year + fips, subsetNEI, sum)
+aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="24510"] <- "Baltimore, MD"
+aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="06037"] <- "Los Angeles, CA"
+png("plot6.png", width=1040, height=480)
+g <- ggplot(aggregatedTotalByYearAndFips, aes(factor(year), Emissions))
+g <- g + facet_grid(. ~ fips)
+g <- g + geom_bar(stat="identity")  +
+  xlab("year") +
+  ylab(expression('Total PM'[2.5]*" Emissions")) +
+  ggtitle('Emissions vehicle (type=ON-ROAD) in Baltimore City, MD (fips = "24510") X Los Angeles, CA (fips = "06037")  1999-2008')
+print(g)
+dev.off()
